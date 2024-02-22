cb1ed1b93823c53864267fa9db9baa311835bb9b
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-02-22 10:50:25 +0200

    Added folders for organizing files

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Restaurant from './screens/Restaurant';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Restaurant />
     </View>
   );
 }
---
+++ screens/Restaurant.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Restaurant() {
+  return (
+    <View>
+      <Text>Restaurant</Text>
+    </View>
+  )
+}
+
