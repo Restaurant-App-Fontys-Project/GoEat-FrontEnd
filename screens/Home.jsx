492b400b58b039bd37f18c3003fe3efbcaa34017
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-03-27 13:39:00 +0200

    added home screen

---
+++ screens/Home.jsx
@@ -0,0 +1,6 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { View, Image, StyleSheet, Text, TouchableOpacity, ScrollView, Dimensions } from "react-native";
+
+export default function Home({ navigation }) {
+}
