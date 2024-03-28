ff7fad4bf1c06a6daa6e3994e51a299ad899ac1d
Shehara <t2wasu00@students.oamk.fi>
2024-03-28 09:03:32 +0200

    added location screen

---
+++ screens/Location.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from "react-native";
+import CalendarPicker from "react-native-calendar-picker";
+import { FontAwesome } from '@expo/vector-icons';
+
+
